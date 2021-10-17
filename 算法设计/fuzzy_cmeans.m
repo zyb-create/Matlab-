a784fff14ea08abf18b553ae85900643dbb61ef0
zyb-create <zyb150410@163.com>
2021-10-17 13:38:14 +0800

    modified:   README.md 	renamed:    "\347\256\227\346\263\225\350\256\276\350\256\241/week5and6/center.txt" -> "\347\256\227\346\263\225\350\256\276\350\256\241/center.txt" 	renamed:    "\347\256\227\346\263\225\350\256\276\350\256\241/week5and6/clusters.txt" -> "\347\256\227\346\263\225\350\256\276\350\256\241/clusters.txt" 	renamed:    "\347\256\227\346\263\225\350\256\276\350\256\241/week5and6/drawFigure.asv" -> "\347\256\227\346\263\225\350\256\276\350\256\241/drawFigure.asv" 	renamed:    "\347\256\227\346\263\225\350\256\276\350\256\241/week5and6/drawFigure.m" -> "\347\256\227\346\263\225\350\256\276\350\256\241/drawFigure.m" 	renamed:    "\347\256\227\346\263\225\350\256\276\350\256\241/week5and6/fuzzy_cmeans.asv" -> "\347\256\227\346\263\225\350\256\276\350\256\241/fuzzy_cmeans.asv" 	renamed:    "\347\256\227\346\263\225\350\256\276\350\256\241/week5and6/fuzzy_cmeans.m" -> "\347\256\227\346\263\225\350\256\276\350\256\241/fuzzy_cmeans.m" 	renamed:    "\347\256\227\346\263\225\350\256\276\350\256\241/week5and6/generateData.asv" -> "\347\256\227\346\263\225\350\256\276\350\256\241/generateData.asv" 	renamed:    "\347\256\227\346\263\225\350\256\276\350\256\241/week5and6/generateData.m" -> "\347\256\227\346\263\225\350\256\276\350\256\241/generateData.m" 	renamed:    "\347\256\227\346\263\225\350\256\276\350\256\241/week5and6/kmeans_singleLink.asv" -> "\347\256\227\346\263\225\350\256\276\350\256\241/kmeans_singleLink.asv" 	renamed:    "\347\256\227\346\263\225\350\256\276\350\256\241/week5and6/kmeans_singleLink.m" -> "\347\256\227\346\263\225\350\256\276\350\256\241/kmeans_singleLink.m" 	new file:   "\347\256\227\346\263\225\350\256\276\350\256\241/kmeans\347\256\227\346\263\225\347\274\272\351\231\267.docx" 	new file:   "\347\256\227\346\263\225\350\256\276\350\256\241/location.txt" 	renamed:    "\347\256\227\346\263\225\350\256\276\350\256\241/week5and6/savedata.m" -> "\347\256\227\346\263\225\350\256\276\350\256\241/savedata.m" 	renamed:    "\347\256\227\346\263\225\350\256\276\350\256\241/week5and6/struct_Test.m" -> "\347\256\227\346\263\225\350\256\276\350\256\241/struct_Test.m" 	renamed:    "\347\256\227\346\263\225\350\256\276\350\256\241/week5and6/test.asv" -> "\347\256\227\346\263\225\350\256\276\350\256\241/test.asv" 	renamed:    "\347\256\227\346\263\225\350\256\276\350\256\241/week5and6/test.m" -> "\347\256\227\346\263\225\350\256\276\350\256\241/test.m" 	deleted:    "\347\256\227\346\263\225\350\256\276\350\256\241/week5and6/location.txt" 	deleted:    "\347\256\227\346\263\225\350\256\276\350\256\241/week5and6/~$x.xlsx" 	renamed:    "\347\256\227\346\263\225\350\256\276\350\256\241/week5and6/x.xlsx" -> "\347\256\227\346\263\225\350\256\276\350\256\241/x.xlsx"

---
+++ 算法设计/fuzzy_cmeans.m
@@ -0,0 +1,88 @@
+clc,clear;
+load location.txt;
+threshold=1e-4;
+n=size(location,1);
+k=5;
+% [clusters,centers]=fuzzy(5,k,n,location,threshold);
+% figure;
+% [clusters,centers]=fuzzy(4,k,n,location,threshold);
+% drawFigure(location,clusters,centers,k,4);
+for i=2:3:5
+    for m=i:i+2
+        subplot(2,3,m-1);
+        [clusters,centers]=fuzzy(m,k,n,location,threshold);
+        drawFigure(location,clusters,centers,k,m);
+        
+    end
+end
+
+function u=randomU(k,n)
+u=zeros(k,n);
+for i=1:k
+    x=randi([1 n+100],1,n);
+    u(i,:)=x./sum(x);
+end
+end
+
+function index=minIndex(dis,k)
+index=1;
+d=dis(1);
+for i=2:k
+    if d>dis(i)
+        index=i;
+        d=dis(i);
+    end
+end
+end
+
+function [c,dis]=calcClusters(k,n,u,m,location)
+% c=zeros(k,2);
+dis=zeros(k,n);
+mu=zeros(k,1);
+u=u.^m;
+c=u*location;
+for i=1:k
+    mu(i)=sum(u(i,:));
+    
+end
+c=c./mu;
+for i=1:k
+    dis(i,:)=sqrt((location(:,1)-c(i,1)).^2+(location(:,2)-c(i,2)).^2);
+end
+% u,c,dis
+end
+
+function [u,jtemp]=calcU(dis,k,n,m,it)
+u=zeros(k,n);
+m=2/(m-1);
+for i=1:k
+    for j=1:n
+        s=(dis(i,j)*(dis(:,j).^(-1))).^m;
+        u(i,j)= 1/sum(s);
+    end
+end
+% for i=1:k
+%     u(i,:)=u(i,:)./sum(u(i,:));
+% end
+jtemp=max(max(u));
+jtemp=jtemp^it;
+% sum(u(1,:))%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+end
+
+function [clusters,centers]=fuzzy(m,k,n,location,th)  %m不能等于1
+clusters=zeros(n,1);
+u=randomU(k,n);
+J=-1;
+jtemp=0;
+it=1;
+while abs(jtemp-J)>th
+    J=jtemp;
+    [centers,dis]=calcClusters(k,n,u,m,location);
+    [u,jtemp]=calcU(dis,k,n,m,it);
+    it=it+1;
+end
+for i=1:n   
+    clusters(i)=find(u(:,i)==max(u(:,i)));
+%    clusters(i)=minIndex(dis(:,i),k); 
+end
+end
